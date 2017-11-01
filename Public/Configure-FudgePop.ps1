72e6a1664676e60904050980eed1ccb629050446
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-11-01 00:11:08 -0400

    Update Configure-FudgePop.ps1

--- Public/Configure-FudgePop.ps1
+++ Public/Configure-FudgePop.ps1
@@ -1,13 +1,25 @@
 #requires -version 3
+#requires -RunAsAdministrator
 <#
+.SYNOPSIS
+	Configure FudgePop options
+.NOTES
+	1.0.3 - 10/31/2017 - David Stein
 #>
 
 function Configure-FudgePop {
 	[CmdletBinding(SupportsShouldProcess=$True)]
 	param ()
+	
+	Write-FPLog $Script:FPVersion
+	Write-FPLog $Script:FPRegRoot
+	Write-FPLog $Script:FPRunJob
+	Write-FPLog $Script:FPCFDefault
+
 	$ControlFile = Get-FPConfiguration -Name "ControlFile" -Default $FPCFDefault
 	$ScheduleHrs = Get-FPConfiguration -Name "ScheduleHours" -Default 1
 	$EnableJob   = Get-FPConfiguration -Name "EnableJob" -Default 1
+	$RunJobName  = Get-FPConfiguration -Name "RunJobName" -Default $Script:FPRunJob
 
 	Write-Host "Current control file is: $ControlFile" -ForegroundColor Cyan
 	$newFile  = (Read-Host "  New control file or Enter to accept default")
@@ -30,19 +42,45 @@ function Configure-FudgePop {
 	Set-FPConfiguration -Name "ScheduleHours" -Data $newHours | Out-Null
 	
 	if ($EnableJob -eq 1) {
-		if (Get-ScheduledTask -TaskName "$FPRunJob" -ErrorAction SilentlyContinue) {
-			# update task
+		#$filepath = "$(Split-Path((Get-Module FudgePop).Path))\Public\Invoke-FudgePop.ps1"
+		$filepath = "$(Split-Path((Get-Module FudgePop).Path))\Public\RunFudgePop.bat"
+		if (Test-Path $filepath) {
+			Write-FPLog "operation: $filepath"
+			Write-FPLog "cmd: SCHTASKS /Create /RU `"SYSTEM`" /SC hourly /MO $newHours /TN `"$RunJobName`" /TR `"$filepath`" /RL HIGHEST /F"
+			#}
+			try {
+				SCHTASKS /Create /RU "SYSTEM" /SC hourly /MO $newHours /TN "$RunJobName" /TR "$filepath" /RL HIGHEST /F
+				if (Get-ScheduledTask -TaskName $RunJobName -ErrorAction SilentlyContinue) {
+					Write-FPLog -Category "Info" -Message "task has been updated successfully."
+					Set-FPConfiguration -Name "ScheduledTaskName" -Data $RunJobName | Out-Null
+					Write-Output $True
+				}
+				else {
+					Write-FPLog -Category "Error" -Message "well, that sucked. no scheduled task update for you."
+				}
+			}
+			catch {
+				Write-FPLog -Category 'Error' -Message $_.Exception.Message
+			}
 		}
 		else {
-			# create task
+			Write-FPLog -Category 'Error' -Message "$filepath could not be found"
 		}
 	}
 	else {
-		if (Get-ScheduledTask -TaskName "$FPRunJob" -ErrorAction SilentlyContinue) {
-			# disable task
+		if (Get-ScheduledTask -TaskName "$RunJobName" -ErrorAction SilentlyContinue) {
+			try {
+				Get-ScheduledTask -TaskName "$RunJobName" -ErrorAction SilentlyContinue |	
+					Unregister-ScheduledTask -Confirm:$False -ErrorAction Stop
+			}
+			catch {
+				Write-FPLog -Category 'Error' -Message $_.Exception.Message
+				Write-Host 'FudgePop scheduled task could not be removed from this computer.  Refer to log file for details.' -ForegroundColor Red
+				break
+			}
 		}
 		else {
-			# do nothing
+			Write-FPLog -Category 'Info' -Message "uhhhhhhhh. There is no scheduled task named $RunJobName to disable. No biggie."
 		}
 	}
 	Write-Host "Configuration has been updated" -ForegroundColor Green
