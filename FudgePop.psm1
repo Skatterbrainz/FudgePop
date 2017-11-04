bac7f373fc3d75b5ad63816f26e1866576ab9792
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-11-03 21:13:57 -0400

    Update FudgePop.psm1

--- FudgePop.psm1
+++ FudgePop.psm1
@@ -1,1 +1,7 @@
+$Script:FPVersion   = '1.0.5'
+$Script:FPRegRoot   = 'HKLM:\SOFTWARE\FudgePop'
+$Script:FPRunJob    = 'FudgePop Agent'
+$Script:FPCFDefault = 'https://raw.githubusercontent.com/Skatterbrainz/FudgePop/master/control.xml'
+$Script:FPLogFile   = "c:\windows\temp\fudgepop.log"
+
 $(Get-ChildItem "$PSScriptRoot" -Recurse -Include "*.ps1").foreach{. $_.FullName}
