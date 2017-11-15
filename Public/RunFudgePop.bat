b4e4c000e68e32fee53d00680cad9ab1d8d95d8f
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-11-15 12:10:37 -0500

    Fixed bug in module path reference
    What a bonehead !!

--- Public/RunFudgePop.bat
+++ Public/RunFudgePop.bat
@@ -1,4 +1,4 @@
 @echo off
 rem RunFudgePop.bat
 rem Version 1.0.10 - 11/14/2017
-PowerShell.exe -ExecutionPolicy ByPass -NoProfile -Command "& {Import-Module C:\users\David\Documents\FudgePop\FudgePop;Invoke-FudgePop -Verbose}" >%SYSTEMROOT%\TEMP\FudgePop.txt
+PowerShell.exe -ExecutionPolicy ByPass -NoProfile -Command "& {Import-Module FudgePop;Invoke-FudgePop -Verbose}" >%SYSTEMROOT%\TEMP\FudgePop.txt
